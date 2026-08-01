f345f913d965a44874d05b78d98361c68e9f3607
SPY YT <JihadShaikh13@gmail.com>
2026-08-01 22:14:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Static files serve karne ke liye
-app.use(express.static(path.join(__dirname, '.')));
+// Yeh line ensure karegi ki index.html file load ho
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
